113e85b79fd617e681c87c53c45cfc5b2050587d
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2018-11-10 02:59:46 -0200

    nav bar melhorado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { AngularFireModule} from 'angularfire2';
 import { AngularFireDatabaseModule} from 'angularfire2/database';
 import { AngularFireAuthModule} from 'angularfire2/auth';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +21,7 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { OrdersComponent } from './admin/orders/orders.component';
 
 @NgModule({
   declarations: [
@@ -28,14 +33,29 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
     CheckOutComponent,
     OrderSuccessComponent,
     MyOrdersComponent,
-    AdminProductsComponent
+    AdminProductsComponent,
+    OrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule,
+    RouterModule.forRoot([
+    {path:'',component: HomeComponent},
+    {path:'products',component: ProductsComponent},
+    {path:'shopping-cart',component: ShoppingCartComponent},
+    {path:'check-out',component: CheckOutComponent},
+    {path:'order-success',component: OrderSuccessComponent},
+    {path:'login',component: LoginComponent},
+    {path:'admin/products',component: AdminProductsComponent},
+    {path:'admin/orders',component: OrdersComponent},
+    {path:'my/orders',component: MyOrdersComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
