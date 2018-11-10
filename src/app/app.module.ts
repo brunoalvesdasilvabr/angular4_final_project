4524e5b7d138e4a82a32a3e19c478c12f4552094
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2018-11-10 19:13:54 -0200

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,7 +47,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     AngularFireAuthModule,
     NgbModule,
     RouterModule.forRoot([
-    {path:'',component: HomeComponent},
+    {path:'',component: ProductsComponent },
     {path:'products',component: ProductsComponent},
     {path:'shopping-cart',component: ShoppingCartComponent},
     {path:'check-out',component: CheckOutComponent},
